99c4c6e45c3881bc33cb485fc4ac7b87a4582441
heablejp <yasuhiro.ueji@gmail.com>
2026-04-19 01:40:24 +0000

    Initialize repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def hello
+    # render html: 'hello, world!'
+    # render html: 'hola, mundo!'
+    render html: '¡Hola, mundo!'
+  end
+
+  def goodbye
+    render html: 'goodbye, world!'
+  end
 end
